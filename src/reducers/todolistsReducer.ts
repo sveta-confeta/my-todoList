c2c94a5f5ad68dac57d5511314ac81a7a3ef9c91
sveta-confeta <_svet_@tut.by>
2022-06-28 16:21:11 +0300

    redux-toolkit todolistsReduser

--- src/reducers/todolistsReducer.ts
+++ src/reducers/todolistsReducer.ts
@@ -1,7 +1,7 @@
 import {v1} from "uuid";
 import {Dispatch} from "redux";
 import {todolistApi} from "../api/ todolist-api";
-import { errorAppMessageAC, RequestStatusType, setAppStatusAC} from "./appReducer";
+import {errorAppMessageAC, RequestStatusType, setAppStatusAC} from "./appReducer";
 import {handleServerNetworkError} from "../utils/error-util";
 import {AxiosError} from "axios";
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
@@ -18,35 +18,44 @@ const initialState: Array<AllTodolistsType> = [
     // {id: todolistsTasksID.todolistID_2, titleTodolist: 'What to read', filter: 'All'},
 ];
 
-const slice=createSlice({
-    name:'todolists',
-    initialState:initialState,
-    reducers:{
-        filteredTaskAC(state,action:PayloadAction<{todolistID: string, value: string}>){
+const slice = createSlice({
+    name: 'todolists',
+    initialState: initialState,
+    reducers: {
+        filteredTaskAC(state, action: PayloadAction<{ todolistID: string, value: string }>) {
+            const index = state.findIndex(fi => fi.id === action.payload.todolistID);
+            state[index].filter=action.payload.value;
+        },
+        removeTodolistAC(state, action: PayloadAction<{ todolistID: string }>) {
+            const index = state.findIndex(fi => fi.id === action.payload.todolistID);
+            if (index > -1) {
+                state.splice(index, 1);
+            }
 
         },
-        removeTodolistAC(state,action:PayloadAction<{todolistID: string}>){
-
+        titleTodolistAC(state, action: PayloadAction<{ todolistID: string, title: string }>) {
+            const index = state.findIndex(fi => fi.id === action.payload.todolistID);
+            state[index].title=action.payload.title;
         },
-        titleTodolistAC(state,action:PayloadAction<{todolistID: string, title: string}>){
-
+        addTodolistsAC(state, action: PayloadAction<{ item: ApiTodolistsType }>) {
+            state.push({...action.payload.item, filter: 'All', disabledStatus: 'failed'})
         },
-        addTodolistsAC(state,action:PayloadAction<{item: ApiTodolistsType}>){
-
+        getTodolistsAC(state, action: PayloadAction<{ todolists: Array<ApiTodolistsType> }>) {
+            return action.payload.todolists.map(m => ({...m, filter: 'All', disabledStatus: 'failed'}))
         },
-        getTodolistsAC(state,action:PayloadAction<{todolists: Array<ApiTodolistsType>}>){
-
-        },
-        disabledStatusTodolistAC(state,action:PayloadAction<{todolistID: string, disabledStatus: RequestStatusType}>){
-
+        disabledStatusTodolistAC(state, action: PayloadAction<{ todolistID: string, disabledStatus: RequestStatusType }>) {
+            const index = state.findIndex(fi => fi.id === action.payload.todolistID);
+            state[index].disabledStatus=action.payload.disabledStatus
         },
     }
 
 });
 
-export const TodolistReducer=slice.reducer;
-export const {filteredTaskAC,removeTodolistAC,titleTodolistAC,addTodolistsAC,
-    getTodolistsAC, disabledStatusTodolistAC}=slice.actions;
+export const TodolistReducer = slice.reducer;
+export const {
+    filteredTaskAC, removeTodolistAC, titleTodolistAC, addTodolistsAC,
+    getTodolistsAC, disabledStatusTodolistAC
+} = slice.actions;
 // export const TodolistReducer = (state: Array<AllTodolistsType> = initialState, action: ActionType): Array<AllTodolistsType> => {
 //     switch (action.type) {
 //         case 'FILTERED-TASK': {
@@ -78,15 +87,15 @@ export const {filteredTaskAC,removeTodolistAC,titleTodolistAC,addTodolistsAC,
 
 
 //export const filteredTaskAC = (todolistID: string, value: string) => ({
-  //  type: 'FILTERED-TASK', todolistID,
- //   value,
+//  type: 'FILTERED-TASK', todolistID,
+//   value,
 //} as const);
 
 //export const removeTodolistAC = (todolistID: string) => ({type: 'REMOVE-TODOLIST', todolistID} as const)
 
 //export const titleTodolistAC = (todolistID: string, title: string) => ({
-  //  type: 'TITLE-TODOLIST',
-    //todolistID, title
+//  type: 'TITLE-TODOLIST',
+//todolistID, title
 //} as const);
 
 //export const addTodolistsAC = (item: ApiTodolistsType) => ({type: 'ADD-TODOLIST', item,} as const); // newTodolistID:v1(), //генерируем 1 id и для тудулист и тасок если нет апи
@@ -94,26 +103,26 @@ export const {filteredTaskAC,removeTodolistAC,titleTodolistAC,addTodolistsAC,
 //export const getTodolistsAC = (todolists: Array<ApiTodolistsType>) => ({type: 'GET-TODOLISTS', todolists,} as const);
 
 //export const disabledStatusTodolistAC = (todolistID: string, disabledStatus: RequestStatusType) => ({
- //   type: 'DISABLED-STATUS',
- //   todolistID, disabledStatus,
+//   type: 'DISABLED-STATUS',
+//   todolistID, disabledStatus,
 //} as const);//disabled buttons
 
-export const todolistsThunk =()=> (dispatch: Dispatch) => {
-    dispatch(setAppStatusAC({value:'loading'}))
+export const todolistsThunk = () => (dispatch: Dispatch) => {
+    dispatch(setAppStatusAC({value: 'loading'}))
     todolistApi.getTodolist().then((res) => { //get запрос за тодолистами
-        dispatch(setAppStatusAC({value:'failed'}))
-        dispatch(getTodolistsAC({todolists:res.data}))
+        dispatch(setAppStatusAC({value: 'failed'}))
+        dispatch(getTodolistsAC({todolists: res.data}))
     })
 }
 
 export const todolistDeleteThunkCreator = (todolistID: string) => (dispatch: Dispatch) => {
-    dispatch(setAppStatusAC({value:'loading'})) //крутилка включилась
-    dispatch(disabledStatusTodolistAC({todolistID:todolistID, disabledStatus:'loading'})); //кнопка дизаблется
+    dispatch(setAppStatusAC({value: 'loading'})) //крутилка включилась
+    dispatch(disabledStatusTodolistAC({todolistID: todolistID, disabledStatus: 'loading'})); //кнопка дизаблется
     todolistApi.deleteTodolist(todolistID)
         .then((res) => { //удаление тодолистов
-            dispatch(setAppStatusAC({value:'failed'}))//крутилка отключилась
+            dispatch(setAppStatusAC({value: 'failed'}))//крутилка отключилась
             // dispatch(disabledStatusTodolistAC(todolistID,'failed')); мы удаляем тодолист, поэтому нет смысла восстанавливать кнопку
-            dispatch(removeTodolistAC({todolistID:todolistID}))
+            dispatch(removeTodolistAC({todolistID: todolistID}))
         })
         .catch((err: AxiosError) => {
             handleServerNetworkError(err, dispatch)
@@ -125,14 +134,14 @@ export const todolistDeleteThunkCreator = (todolistID: string) => (dispatch: Dis
 export const todolistAddThunkCreator = (title: string) => {
     return async (dispatch: Dispatch) => {
         try {
-            dispatch(setAppStatusAC({value:'loading'}))
+            dispatch(setAppStatusAC({value: 'loading'}))
             const res = await todolistApi.createNewTodolist(title)
             if (res.data.resultCode === 0) {
-                dispatch(setAppStatusAC({value:'failed'}))
-                dispatch(addTodolistsAC({item:res.data.data.item}))
+                dispatch(setAppStatusAC({value: 'failed'}))
+                dispatch(addTodolistsAC({item: res.data.data.item}))
             } else {
-                dispatch(setAppStatusAC({value:'failed'}))
-                dispatch(errorAppMessageAC({value:res.data.messages[0]})); //достаем из массива сообщение об ошибке
+                dispatch(setAppStatusAC({value: 'failed'}))
+                dispatch(errorAppMessageAC({value: res.data.messages[0]})); //достаем из массива сообщение об ошибке
             }
         } catch (err) {
             let error = err as AxiosError
@@ -142,19 +151,19 @@ export const todolistAddThunkCreator = (title: string) => {
 }
 
 export const titleTodolistThunkCreator = (todolistID: string, title: string) => (dispatch: Dispatch) => {
-    dispatch(setAppStatusAC({value:'loading'}))
+    dispatch(setAppStatusAC({value: 'loading'}))
     todolistApi.updateTodoTitle(todolistID, title)
         .then((res) => {
             if (res.data.resultCode === 0) {
-                dispatch(setAppStatusAC({value:'failed'}))
-                dispatch(titleTodolistAC({todolistID:todolistID, title:title}))
+                dispatch(setAppStatusAC({value: 'failed'}))
+                dispatch(titleTodolistAC({todolistID: todolistID, title: title}))
             } else {
-                dispatch(setAppStatusAC({value:'failed'}))
-                dispatch(errorAppMessageAC({value:res.data.messages[0]})); //достаем из массива сообщение об ошибке
+                dispatch(setAppStatusAC({value: 'failed'}))
+                dispatch(errorAppMessageAC({value: res.data.messages[0]})); //достаем из массива сообщение об ошибке
             }
         })
         .catch((err: AxiosError) => {
-            handleServerNetworkError(err,dispatch)
+            handleServerNetworkError(err, dispatch)
         })
 }
 //types
